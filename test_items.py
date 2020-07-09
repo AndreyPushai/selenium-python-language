d066a5f85c72d73a8809c150b051ef38d5aa4937
AndreyPushai <andrey.pushai@gmail.com>
2020-07-09 18:50:35 +0300

    Changed link in test

--- test_items.py
+++ test_items.py
@@ -2,6 +2,6 @@ import pytest
 from selenium import webdriver
 
 def test_add_to_basket_button_present(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/")
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
     assert button, "No such element exception"
