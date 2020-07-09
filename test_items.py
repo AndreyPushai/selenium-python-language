58eece4b371fb12a66287c9f0fb8d8dc0f251ee5
AndreyPushai <andrey.pushai@gmail.com>
2020-07-09 18:45:12 +0300

    Assert to -add to basket- button added

--- test_items.py
+++ test_items.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 
 def test_add_to_basket_button_present(browser):
     browser.get("http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/")
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert button, "No such element exception"
