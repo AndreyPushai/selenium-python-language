29d639fd947431303e1105e0ba3f35e088a178ca
AndreyPushai <andrey.pushai@gmail.com>
2020-07-14 11:54:46 +0300

    Removed environment pytest -.pyc- files

--- test_items.py
+++ test_items.py
@@ -1,7 +1,9 @@
 import pytest
+import time
 from selenium import webdriver
 
 def test_add_to_basket_button_present(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(5)
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
     assert button, "No such element exception"
